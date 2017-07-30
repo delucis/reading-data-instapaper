5b9e0599ded93a213b722e3b17c4f15f3395f9f9
delucis <swithinbank@gmail.com>
2017-07-30 20:11:22 +0200

    Create index.js with module skeleton.

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @module reading-data-instapaper
+ */
+
+const instapaper = require('instapaper')
+const log = require('winston')
+
+const ReadingDataInstapaper = (function () {
+  return {
+  }
+}())
+
+module.exports = ReadingDataInstapaper
